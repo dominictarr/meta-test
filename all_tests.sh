321e1ffea6d1301f3affbec19c503b4d8d4e6c3e
Dominic Tarr <dominic.tarr@gmail.com>
2011-06-06 12:36:57 -0700

    update test script

--- all_tests.sh
+++ all_tests.sh
@@ -1,9 +1,11 @@
 node test/arg-parser.node.js && \
 node test/asynct.node.js && \
+node test/asynct.setup-teardown .node.js && \
 node test/expresso.node.js && \
 node test/nodeunit.node.js && \
 node test/platform.node.js && \
 node test/report.node.js && \
+node test/timeout.node.js && \
 node test/runner.node.js && \
 node test/selector.node.js && \
 node test/synct.node.js && \
